198ef2aa5b457b864834c2d04e5a632f90b8d958
junaidapdev <junaidap.dev.com>
2022-06-22 12:56:17 +0530

    user card component added

---
+++ src/components/UserCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Card, CardBody} from "reactstrap"
+
+const UserCard = ([user]) => {
+  return (
+        <Card className='text-center mt-3 mb-4'>
+             <img src={user.avatar.url} className="img-thumbnail" alt='propic'/>
+             <CardBody>
+                <div className='text-primary'>{user.name}</div>
+                <div className='text-primary'>{user.location}</div>
+                <div className='text-primary'>{user.bio}</div>
+                <div className='text-info'>Available for hire: {user.hireable ? "Yes" : "Nope"}</div>
+                <div className='text-info'>Followers: {user.followers}</div>
+             </CardBody>
+        </Card>
+    )
+}
+
+export default UserCard
